794cf38aaaaf23206fd30e67c4e14747a810a9ce
Lokarin <onukiamaral@protonmail.com>
2024-02-22 14:48:35 -0300

    atividade10-1

--- prog2/atividade10-1.c
+++ prog2/atividade10-1.c
@@ -3,26 +3,32 @@
 
 void reorder(int *x) {
 	int temp = 0;
+
+	if (x[0] == x[1] == x[2]) {
+		puts("Numeros iguais (0)");
 	
-	puts("Numeros iniciais: ");
-	for (int i = 0; i < 3; i++) {
-		printf("%c: %i\n",('a'+i),x[i]);
-	
-	}
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (x[j]>x[i]) {
-				temp = x[i];
-				x[i] = x[j];
-				x[j] = temp;
+	} else {
+		puts("Numeros diferentes: ");
+		puts("Numeros iniciais: ");
+		for (int i = 0; i < 3; i++) {
+			printf("%c: %i\n",('a'+i),x[i]);
+		
+		}
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (x[j]>x[i]) {
+					temp = x[i];
+					x[i] = x[j];
+					x[j] = temp;
+				}
 			}
 		}
-	}
-	
-	puts("Numeros finais: ");
-	for (int i = 0; i < 3; i++) {
-		printf("%c: %i\n",('a'+i),x[i]);
-	
+		
+		puts("Numeros finais: ");
+		for (int i = 0; i < 3; i++) {
+			printf("%c: %i\n",('a'+i),x[i]);
+		
+		}
 	}
 }
 
